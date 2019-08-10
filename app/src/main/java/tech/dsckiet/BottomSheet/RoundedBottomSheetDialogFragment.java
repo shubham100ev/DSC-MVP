1ea5eb47d3a5d5be9d67fe63229883a55b64a449
Anshul1507 <ag1507anshul@gmail.com>
2019-08-10 20:26:03 +0530

    Added Rounded Bottom Sheet (Bottom Navigation)

---
+++ app/src/main/java/tech/dsckiet/BottomSheet/RoundedBottomSheetDialogFragment.java
@@ -0,0 +1,35 @@
+package tech.dsckiet.BottomSheet;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetDialog;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.support.design.widget.CoordinatorLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import tech.dsckiet.R;
+
+public class RoundedBottomSheetDialogFragment extends BottomSheetDialogFragment {
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(BottomSheetDialogFragment.STYLE_NORMAL, R.style.BottomSheetDialogTheme);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_bottom_sheet_nav,container,false);
+
+        return view;
+    }
+
+}
--- app/src/main/java/tech/dsckiet/MainActivity.java
+++ app/src/main/java/tech/dsckiet/MainActivity.java
@@ -1,13 +1,55 @@
 package tech.dsckiet;
 
+import android.support.design.bottomappbar.BottomAppBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import tech.dsckiet.BottomSheet.RoundedBottomSheetDialogFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RoundedBottomSheetDialogFragment bottomSheetDialog;
+    BottomAppBar bottomAppBar;
+    private int selectedFragmentIndex = 1;
+
+    //TODO:: AttachBaseContext()
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomAppBar = findViewById(R.id.bottom_app_bar);
+
+        setSupportActionBar(bottomAppBar);
+
+//        if(savedInstanceState == null){
+//            updateFragment(selectedFragmentIndex);
+//        }
+
+        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showBottomSheet();
+            }
+        });
+
+
+    }
+
+
+    private void showBottomSheet(){
+        bottomSheetDialog = new RoundedBottomSheetDialogFragment();
+        if(getApplicationContext()!= null){
+//            BottomSheetDialog dialog = new BottomSheetDialog(getContext(),R.style.BottomSheetDialogTheme);
+//            bottomSheetDialog.setStyle(R.style.BottomSheetDialogTheme);
+
+            bottomSheetDialog.show(getSupportFragmentManager(),"bottom sheet");
+        }
+//        bottomSheetDialog.show(getSupportFragmentManager(),"bottom sheet");
+    }
+    private void updateFragment(int index){
+
     }
 }
