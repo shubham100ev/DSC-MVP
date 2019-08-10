8aff226b62315c1be2f95c1bb4fee1cc8dda1a97
Anshul1507 <ag1507anshul@gmail.com>
2019-08-11 00:34:06 +0530

    Added Click listener for bottom nav for all fragments
    added basic fragment layout for all menu options

--- app/src/main/java/tech/dsckiet/MainActivity.java
+++ app/src/main/java/tech/dsckiet/MainActivity.java
@@ -4,14 +4,24 @@ import android.support.design.bottomappbar.BottomAppBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import tech.dsckiet.About.AboutFragment;
 import tech.dsckiet.BottomSheet.RoundedBottomSheetDialogFragment;
+import tech.dsckiet.CodeConduct.CodeConductFragment;
+import tech.dsckiet.Events.EventsFragment;
+import tech.dsckiet.Projects.ProjectFragment;
+import tech.dsckiet.Stories.StoriesFragment;
+import tech.dsckiet.Team.TeamFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RoundedBottomSheetDialogFragment.NavClickListener {
+
 
     private RoundedBottomSheetDialogFragment bottomSheetDialog;
     BottomAppBar bottomAppBar;
-    private int selectedFragmentIndex = 1;
+
+
+    public static int selectedFragmentIndex = 1;
 
     //TODO:: AttachBaseContext()
 
@@ -24,9 +34,9 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(bottomAppBar);
 
-//        if(savedInstanceState == null){
-//            updateFragment(selectedFragmentIndex);
-//        }
+        if(savedInstanceState == null){
+            updateFragment(selectedFragmentIndex);
+        }
 
         bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,14 +52,45 @@ public class MainActivity extends AppCompatActivity {
     private void showBottomSheet(){
         bottomSheetDialog = new RoundedBottomSheetDialogFragment();
         if(getApplicationContext()!= null){
-//            BottomSheetDialog dialog = new BottomSheetDialog(getContext(),R.style.BottomSheetDialogTheme);
-//            bottomSheetDialog.setStyle(R.style.BottomSheetDialogTheme);
-
+            bottomSheetDialog.navclickListener = this;
             bottomSheetDialog.show(getSupportFragmentManager(),"bottom sheet");
         }
-//        bottomSheetDialog.show(getSupportFragmentManager(),"bottom sheet");
     }
-    private void updateFragment(int index){
 
+    private void updateFragment(int index){
+        switch (index){
+            case 1:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, new ProjectFragment()).commit();
+                break;
+            case 2:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, new TeamFragment()).commit();
+                break;
+            case 3:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, new StoriesFragment()).commit();
+                break;
+            case 4:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, new EventsFragment()).commit();
+                break;
+            case 5:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, new CodeConductFragment()).commit();
+                break;
+            case 6:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, new AboutFragment()).commit();
+                break;
+            default:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, new AboutFragment()).commit();
+                break;
+        }
+    }
+
+
+     public void onNavItemClicked(int index){
+        try {
+            bottomSheetDialog.dismiss();
+            selectedFragmentIndex = index;
+            updateFragment(index);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
