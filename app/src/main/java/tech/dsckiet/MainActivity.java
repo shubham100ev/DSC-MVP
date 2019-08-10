e70aecb730503231931b4367eae2d466fe78e276
Anshul1507 <ag1507anshul@gmail.com>
2019-08-10 11:27:34 +0530

    Migrating from android.x to appcompat
    Remove androidx artificats

--- app/src/main/java/tech/dsckiet/MainActivity.java
+++ app/src/main/java/tech/dsckiet/MainActivity.java
@@ -1,7 +1,6 @@
 package tech.dsckiet;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
